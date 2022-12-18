2d6ab0d9ea668f5c235e039dc64f7a7c19837a0c
Otton Lucena <ottonlucena@gmail.com>
2022-12-18 12:46:37 -0300

    primer commit

---
+++ ejercicioLista.js
@@ -0,0 +1,10 @@
+const date = ["Otton", 28, false, new Date("January 05 1995")];
+const libro = {
+  titulo: "Eloquent",
+  autor: "Marjin Haverbeke",
+  fecha: new Date("January 05 1995"),
+  url: "https://eloquentjavascript.net/",
+};
+
+console.log(libro);
+console.log(date);
