916404f5394f2ec79fa348952593fcdada71efbb
Otton Lucena <ottonlucena@gmail.com>
2022-12-22 23:34:27 -0300

    Ejercicio 5 Numeros

---
+++ ejercicioNumero.js
@@ -0,0 +1,13 @@
+const alturaCentrimetros = 172;
+const alturaMetros = 1.72;
+const pesoKG = 81.1;
+
+const alturaRedondeada = Math.round(alturaMetros);
+console.log(alturaRedondeada);
+
+const pesoRedondeado = Math.floor(pesoKG);
+console.log(pesoRedondeado);
+
+let max = Number.MAX_VALUE;
+console.log(max + 1);
+// Si es el maximo valor que se puede obtener sumando.
