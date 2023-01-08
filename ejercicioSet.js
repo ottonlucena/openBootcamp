888a39340b04090e3d33bd71571c7fd6d84db95c
Otton Lucena <ottonlucena@gmail.com>
2023-01-08 11:01:19 -0300

    Agregando ejercicio de SET

---
+++ ejercicioSet.js
@@ -0,0 +1,11 @@
+const newSet = new Set(["Brianny", "Ana", "Eukaris", "Jesus", "Otton"]);
+
+console.log(newSet);
+
+newSet.add("Otton");
+
+console.log(newSet);
+
+newSet.add("JavaScript");
+
+console.log(newSet);
